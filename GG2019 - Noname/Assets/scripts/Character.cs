a020d8c1ae454bb192cb152c05192687644bcadd
scythemaster <guillaume.rivoire@epitech.eu>
2019-01-26 12:03:41 +0100

    bodies

---
+++ GG2019 - Noname/Assets/scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int health = 100;
+        int attack = 10;
+        int defense = 10;
+        int speed = 1;
+    }
+}
