19085dea30ea166099f9007d14fe5134ce705630
scythemaster <guillaume.rivoire@epitech.eu>
2019-01-26 14:46:40 +0100

    img + ennemy empty

---
+++ GG2019 - Noname/Assets/Scripts/Ennemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+
+    [SerializeField]
+    float Speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardPlayer();
+    }
+
+    void MoveTowardPlayer()
+    {
+        
+    }
+}
