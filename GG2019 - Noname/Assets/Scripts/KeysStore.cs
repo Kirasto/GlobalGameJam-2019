7321a2433957c3a59a7471a2f4501e07082d35b8
kirasto <benoit.doyen@epitech.eu>
2019-01-26 13:31:07 +0100

    player move

---
+++ GG2019 - Noname/Assets/Scripts/KeysStore.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Key
+{
+    public string name;
+    public KeyCode keyCode;
+
+    public Key(string name, KeyCode keyCode)
+    {
+        this.name = name;
+        this.keyCode = keyCode;
+    }
+}
+
+public class KeysStore : MonoBehaviour
+{
+    public static KeyCode GetKeyCode(string name)
+    {
+        if (instance != null)
+        {
+            Key key = new Key("None", KeyCode.None);
+
+            instance.keys.TryGetValue(name, out key);
+            return key.keyCode;
+        }
+        return KeyCode.None;
+    }
+
+    public static KeysStore instance;
+    public Dictionary<string, Key> keys;
+
+    private void AddKey(string name, KeyCode keyCode)
+    {
+        keys.Add(name, new Key(name, keyCode));
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        keys = new Dictionary<string, Key>();
+        AddKey("Up", KeyCode.Z);
+        AddKey("Down", KeyCode.S);
+        AddKey("Left", KeyCode.Q);
+        AddKey("Right", KeyCode.D);
+    }
+}
---
+++ GG2019 - Noname/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 1f;
+    [SerializeField]
+    bool canMove;
+
+    [SerializeField]
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        canMove = true;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (!canMove)
+            return;
+
+        Vector3 velocity = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeysStore.GetKeyCode("Up")))
+        {
+            velocity.z += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeysStore.GetKeyCode("Down")))
+        {
+            velocity.z -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeysStore.GetKeyCode("Left")))
+        {
+            velocity.x -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeysStore.GetKeyCode("Right")))
+        {
+            velocity.x += moveSpeed * Time.deltaTime;
+        }
+
+        transform.position += velocity;
+    }
+}
